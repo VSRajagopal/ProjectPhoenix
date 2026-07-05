becaaccb881e5c5b57663ded3c60125023777bcb
Raja The Winner <winnersmindset47@gmail.com>
2026-07-05 18:52:06 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/projectphoenix/PhoenixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.projectphoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoenixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectphoenix/PhoenixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectphoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
